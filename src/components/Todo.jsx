3d79e12b1b6d33f229101a7edfc8bbfa1ac8454b
Orianaqh <orianaqh1583@gmail.com>
2023-01-10 08:57:17 +0100

    itemsStyle,adButton,icons

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+// ICONS
+import { AiTwotoneEdit,AiFillDelete,AiFillCheckCircle } from "react-icons/ai";
 
 
 function Todo({item, onUpdate, onDelete, changeStatus}) {
   const [isEdit, setIsEdit] = useState(false);
 
 
-  // ------------------- component - lass uns Item verändern
+// EDIT ITEM
   function FormEdit(){
     const [newValue, setNewValue] = useState(item.name);
 
@@ -13,36 +15,53 @@ function Todo({item, onUpdate, onDelete, changeStatus}) {
       e.preventDefault();
     };
 
-  // ------------------- component - lass uns Item verändern
     function handleClickUpdateTodo(){
       onUpdate(item.id, newValue);
       setIsEdit(false)
     };
 
     return(
-      <form className="todoUpdateForm" onSubmit={handleSubmit}>
-        <input type='text' className="todoInput" value={newValue} onChange={(e) => setNewValue(e.target.value)}/>
-        <button className="btn btn-primary me-md-2" onClick={handleClickUpdateTodo}>L</button>
+      <form
+        aria-label="updateEdit"
+        className=""
+        onSubmit={handleSubmit}>
+        <input
+          type='text'
+          className="todoInput"
+          value={newValue}
+          onChange={(e) => setNewValue(e.target.value)}/>
+        <button
+          className="btn btn-primary me-md-2"
+          onClick={handleClickUpdateTodo}><AiFillCheckCircle/>
+        </button>
       </form>
     );
   }
 
-  // ------------------- component - addiert item
+  // AD ITEM 
   function TodoElement(){
     return (
-      <ul className="">
-        <section className="itemStyle">
+      <ul
+        aria-label="todo"
+        className="shadow-inner bg-background mb-1 rounded-lg">
           <div className={item.completed ? "true" : "false"}>
-            <li className="list-group-item" onClick={() => changeStatus(item)} >{item.name} 
+            <li
+              className="p-1 flex items-center justify-between"
+              onClick={() => changeStatus(item)} >{item.name} 
             <div className="">
-              <button className="btn btn-primary me-md-2" type="button" onClick={() => setIsEdit(true)}>E</button>
-              <button className="btn btn-secondary" onClick={() => onDelete(item.id)}>X</button>
+              <button
+                className="btn btn-primary me-md-2"
+                type="button"
+                onClick={() => setIsEdit(true)}><AiTwotoneEdit/>
+              </button>
+              <button
+                className="btn btn-secondary"
+                onClick={() => onDelete(item.id)}><AiFillDelete/>
+              </button>
             </div>
             </li>
           </div>
-        </section>
-        
-      
+
       </ul>
       );
   }
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import Todo from './Todo';
 import moment from 'moment';
 
+// Icons
+import { BsPlusCircleFill } from 'react-icons/bs'
+
 const date = moment().format('MMM [\r\n] D');
 const time = moment().format('LT');
 
@@ -51,31 +54,35 @@ export function TodoApp() {
   return (
     <div
       className='
-        w-auto
+        w-auto h-[900px]
         my-[80px]
         p-10
         bg-white border-radius rounded-[30px]
         flex flex-col justify-around items-center gap-10
         '>
       
-      <div className='text-4xl text-darkRosa'>
-        <h1 className='font-bold'>Hi, You!</h1>
-        <h3 className='font-light'>you have something...</h3>
+      <div className='text-4xl'>
+        <h1 className='font-bold text-darkRosa'>Hi, You!</h1>
+        <h3 className='font-light opacity-50'>you have something...</h3>
       </div>
 
       <div
         aria-label='logoAndDate'
-        className="flex flex-row gap-[100px]">
+        className="
+          flex flex-row items-center
+          gap-[80px]
+          bg-lightRosa p-4 rounded-3xl
+          ">
         <h2
           aria-label='logo'
           className='
             p-5 rounded-full
-            font-bold text-white text-xl
-            bg-background'
+            font-bold text-background text-xl
+            bg-white'
           >To do<br></br>today!</h2>
         <h2
           aria-label='date'
-          className='text-background
+          className='text-white
             font-bold text-7xl'
           >{date}</h2>
 
@@ -83,21 +90,25 @@ export function TodoApp() {
 
       <div
         aria-label='inputAndToDos'>
-        <form className=''>
+        <form
+          className='flex flex-row items-center justify-center gap-5 mb-[30px]'>
+        {/* INPUT TODO */}
           <input 
           aria-label='todoInput'
+          className=' w-full p-2
+            rounded-[1rem] focus:outline-lightGray
+            border border-lightGray
+            '
           type='text' 
-          className='mb-[30px] w-full'
-          placeholder="Add todo..." 
           value={title}
           onChange={(e) => setTitle(e.target.value)}/>
 
-          <button
-            aria-label='buttonCreate'
-            className='absolute' 
-            onClick={addTodo}>
-            +
-          </button>
+        {/* AdButton TODO */}
+        <button
+          className=''
+          onClick={addTodo}
+          ><BsPlusCircleFill className='w-[30px] h-[30px] text-lightGray'/>
+        </button>
         </form>
 
         <div
@@ -105,7 +116,8 @@ export function TodoApp() {
           className='w-[400px] flex-col'>
           {todos.map(item => {
             return(
-              <Todo 
+              <Todo
+              className=''
               key={item.id}
               item={item}
               onUpdate={handleClickUpdateTodo}
@@ -121,13 +133,14 @@ export function TodoApp() {
       {/* DELETE BUTTON AND TIME */}
     <div
       aria-label='deleteButtonAndTime'
-      className='flex flex-col justify-center items-center'>
+      className='flex flex-col
+        justify-center items-center'>
       {/* DELETE BUTTON */}
         <div
           aria-label='buttonDeleteAll'
           className='w-[10em] p-3 rounded-full
             flex justify-center
-            bg-darkRosa 
+            bg-darkRosa text-white
           '>
           <button className="btn btn-secondary" onClick={deleteAll}>Delete all</button>
         </div>
